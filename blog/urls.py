61424ac3b18b4faac5354843eae674231a2ef366
sanketag <40177936+sanketag@users.noreply.github.com>
2020-06-13 10:05:59 +0530

    Add files via upload

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index),
+    path('addblog/',views.addblog),
+    path('addblog1/',views.addblog1),
+    path('allblog/',views.allblog),
+]
