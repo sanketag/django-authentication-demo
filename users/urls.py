6cfda9c687fd9bb6f96c87aeb79ea17dcc33ab7e
sanketag <40177936+sanketag@users.noreply.github.com>
2020-06-13 10:06:44 +0530

    Add files via upload

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index),
+    path('login/',views.login),
+    path('afterlogin/',views.afterlogin),
+    path('signup/',views.signup),
+    path('aftersign/',views.Aftersign.as_view()),
+    path('forgot/',views.forgot),
+    path('logout/',views.logout),
+]
